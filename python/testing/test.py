9c5045934d756649ac2f2c77be02593dbf408edd
Senaxius <alexander.minor004@gmail.com>
2023-02-19 18:53:45 +0100

    1080p testing

--- python/testing/test.py
+++ python/testing/test.py
@@ -5,24 +5,16 @@ from imutils.video import FPS
 import argparse
 import imutils
 import cv2
+import time
 
-# created a *threaded* video stream, allow the camera sensor to warmup,
-# and start the FPS counter
-print("[INFO] sampling THREADED frames from webcam...")
-vs = WebcamVideoStream(src=0).start()
-fps = FPS().start()
-# loop over some frames...this time using the threaded stream
-while (1):
-	# grab the frame from the threaded video stream and resize it
-	# to have a maximum width of 400 pixels
-    frame = vs.read()
-    cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1) & 0xFF
-    fps.update()
 
-fps.stop()
-print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-# do a bit of cleanup
-cv2.destroyAllWindows()
-vs.stop()
+print(cv2.getBuildInformation())
+# vs = cv2.VideoCapture("gst-launch-1.0 -v videotestsrc ! video/x-raw,width=1920,heigh=1080 ! videoconvert ! video/x-raw ! appsink", cv2.CAP_GSTREAMER)
+
+# time.sleep(1)
+
+# _, frame = vs.read()
+# cv2.imshow("test", frame)
+
+# while (1):
+#     pass
--- python/testing/tracker.py
+++ python/testing/tracker.py
@@ -15,50 +15,47 @@ def ball_scanner(frame, buffer, colors = []):
         mask = cv2.inRange(hsv, color.start_color, color.end_color)
         mask = cv2.erode(mask, None, iterations=3)
         mask = cv2.dilate(mask, None, iterations=3)
-        #     # find contours in the mask and initialize the current
-        #     # (x, y) center of the ball
-            # cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            # cnts = imutils.grab_contours(cnts)
-        #     center = None
-        #     # only proceed if at least one contour was found
-        #     if len(cnts) > 0:
-        #         # find the largest contour in the mask, then use
-        #         # it to compute the minimum enclosing circle and
-        #         # centroid
-        #         c = max(cnts, key=cv2.contourArea)
-        #         ((x, y), radius) = cv2.minEnclosingCircle(c)
-        #         # x = 330.
-        #         # y = 225.
-        #         M = cv2.moments(c)
-        #         center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-        #         # center = (330, 225)
-        #         # only proceed if the radius meets a minimum size
-        #         if radius > 10:
-        #             # draw the circle and centroid on the frame,
-        #             # then update the list of tracked points
-        #             # cv2.circle(frame, (int(x), int(y)), int(radius),
-        #             cv2.circle(frame, (int(x), int(y)), int(radius),
-        #                         (0, 255, 255), 2)
-        #             cv2.circle(frame, center, 5, (0, 0, 255), -1)
-                
-        #         # publish on topic
-        #         self.publish_coordinates(x, y, frame.shape[1], frame.shape[0], True, fps)
-        #     else:
-        #         self.publish_coordinates(0.0, 0.0, frame.shape[1], frame.shape[0], False, fps)
+        # find contours in the mask and initialize the current
+        # (x, y) center of the ball
+        contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours = imutils.grab_contours(contours)
+        center = None
+        objects = []
+        # only proceed if at least one contour was found
+        if len(contours) > 0:
+            for contour in contours:
+                ((x, y), radius) = cv2.minEnclosingCircle(contour)
+                M = cv2.moments(contour)
+                center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+                if radius > 1:
+                    objects.append(center)
 
-        #     # update the points queue
-        #     buffer_pts.appendleft(center)
-        #         # loop over the set of tracked points
-        #     for i in range(1, len(buffer_pts)):
-        #         # if either of the tracked points are None, ignore
-        #         # them
-        #         if buffer_pts[i - 1] is None or buffer_pts[i] is None:
-        #             continue
-        #         # otherwise, compute the thickness of the line and
-        #         # draw the connecting lines
-        #         thickness = int(np.sqrt(buffer_size / float(i + 1)) * 2.5)
-        #         cv2.line(frame, buffer_pts[i - 1],
-        #                 buffer_pts[i], (0, 0, 255), thickness)
+        print(objects)
+        #         # draw the circle and centroid on the frame,
+        #         # then update the list of tracked points
+        #         # cv2.circle(frame, (int(x), int(y)), int(radius),
+        #         cv2.circle(frame, (int(x), int(y)), int(radius),
+        #                     (0, 255, 255), 2)
+        #         cv2.circle(frame, center, 5, (0, 0, 255), -1)
+            
+        #     # publish on topic
+        #     self.publish_coordinates(x, y, frame.shape[1], frame.shape[0], True, fps)
+        # else:
+        #     self.publish_coordinates(0.0, 0.0, frame.shape[1], frame.shape[0], False, fps)
+
+        # # update the points queue
+        # buffer_pts.appendleft(center)
+        #     # loop over the set of tracked points
+        # for i in range(1, len(buffer_pts)):
+        #     # if either of the tracked points are None, ignore
+        #     # them
+        #     if buffer_pts[i - 1] is None or buffer_pts[i] is None:
+        #         continue
+        #     # otherwise, compute the thickness of the line and
+        #     # draw the connecting lines
+        #     thickness = int(np.sqrt(buffer_size / float(i + 1)) * 2.5)
+        #     cv2.line(frame, buffer_pts[i - 1],
+        #             buffer_pts[i], (0, 0, 255), thickness)
 
     return frame, mask
 
@@ -75,7 +72,12 @@ if __name__ == "__main__":
     buffer = deque(maxlen=buffer_size)
 
     # create camera capture
-    cap = VideoStream(src=device, resolution=(1920, 1080), framerate=60).start() 
+    # cap = VideoStream(src=device, resolution=(1920, 1080), framerate=30).start() 
+    # cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture("v4l2src device=/dev/video0 ! image/jpeg,framerate=30/1,width=1920, height=1080,type=video ! jpegdec ! videoconvert ! video/x-raw ! appsink", cv2.CAP_GSTREAMER)
+    # cap.set(5, 30)
+    # cap.set(3, 1920)
+    # cap.set(4, 1080)
     framerate = 60
 
     prev_frame_time = 0
@@ -88,31 +90,31 @@ if __name__ == "__main__":
     green_ball.end_color = (63, 255, 255)
 
     # main loop
-    while True:
+    while (cap.isOpened):
         # start time for frameratie limitting
-        start_time = time.time()
+        # start_time = time.time()
         # grab the current frame
-        frame = cap.read()
+        ret, frame = cap.read()
         # resize the frame for faster tracking
         # frame = imutils.resize(frame, width=600)
 
-        tracking_time = time.time()
-        frame, mask = ball_scanner(frame, buffer, colors=[green_ball])
+        # tracking_time = time.time()
+        # frame, mask = ball_scanner(frame, buffer, colors=[green_ball])
         # print(int((time.time()-tracking_time) * 10000))
 
         # show frame (testing only)
         cv2.imshow("Frame", frame)
-        cv2.imshow("mask", mask)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        # if ret:
+        # else:
+        #     print("no")
+        # cv2.imshow("mask", mask)
+        if cv2.waitKey(25) & 0xFF == ord('q'):
             break
-
-
-
         # sleeping to maintain framerate
-        time.sleep(max((1 / framerate) - (time.time() - start_time), 0))
+        # time.sleep(max((1 / framerate) - (time.time() - start_time), 0))
 
-        new_frame_time = time.time()
-        fps = 1/(new_frame_time-prev_frame_time)
-        prev_frame_time = new_frame_time
-        fps = int(fps)
-        print(fps)
+        # new_frame_time = time.time()
+        # fps = 1/(new_frame_time-prev_frame_time)
+        # prev_frame_time = new_frame_time
+        # fps = int(fps)
+        # print(fps)
